2b2c5bc9e4fecf0eefa64ccd153e7b9e2ac269d8
肖泽鹏 <haloxzp@163.com>
2016-04-01 14:03:26 +0800

    Initial Commit

---
+++ 10-GITHub/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  10-GITHub
+//
+//  Created by 大鹏 on 16/4/1.
+//  Copyright © 2016年 dongchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 10-GITHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  10-GITHub
+//
+//  Created by 大鹏 on 16/4/1.
+//  Copyright © 2016年 dongchen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
